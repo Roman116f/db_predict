ff8d57cb72be2e1c349e30b718efa16cb5e4b44f
Roman Langolf <161879590+Roman116f@users.noreply.github.com>
2024-06-18 15:39:42 +0200

    Update presentation.py

--- presentation.py
+++ presentation.py
@@ -3,13 +3,19 @@ import streamlit as st
 st.set_page_config(page_title="Flug Tracking: FRA - OSL", layout="wide")
 
 def show_overview():
-    st.title("Flug Tracking: Frankfurt - Oslo")
+    st.title("Flight Status - Fetching Live API Data")
     st.markdown("""
+    English:
+    This AWS Lambda function retrieves flight data and aircraft information in real time and stores it in a cloud database (MongoDB). 
+    Current flight data (departure time, arrival time, flight status, etc.) is retrieved from the Lufthansa API.
+    The ADSBexchange API provides live data from the aircraft. Based on the positions of the airport and the aircraft and using the Haversine formula, the current position of the aircraft is calculated and the flight route is displayed
+    
+    Deutsch:
     Diese AWS Lambda-Funktion ruft in Echtzeit Flugdaten und Flugzeuginformationen ab und speichert sie in einer Cloud-Datenbank (MongoDB). 
     Aktuelle Flugdaten (Abflugzeit, Ankunftszeit, Flugstatus usw.) werden von der Lufthansa-API abgerufen.
     Die ADSBexchange-API liefert Live-Daten des Flugzeugs. Basierend auf den Positionen des Flughafens und des Flugzeugs sowie unter Anwendung der Haversine-Formel wird die aktuelle Position des Flugzeugs berechnet und die Flugroute dargestellt (siehe unten).
     """)
-
+    st.sidebar.image("lufthansa_flug.png", use_column_width=False)
     # Lufthansa-Logo und Titel hinzufügen
     #st.markdown(
     #    """
